1280c980698aebf2280d0582465a549c8374cc4e
Niklas Mähler <niklas.mahler@regionvasterbotten.se>
2022-12-10 12:42:50 +0100

    Day 9, part 2

--- src/day09.go
+++ src/day09.go
@@ -78,36 +78,72 @@ func (p Pos) touching(other Pos) bool {
     return !(abs(diff_x) > 1 || abs(diff_y) > 1)
 }
 
+func (p Pos) step_towards(other Pos) Pos {
+    if p.touching(other) {
+        return p
+    }
+
+    new_pos := Pos{p.x, p.y}
+
+    x_diff := other.x - p.x
+    y_diff := other.y - p.y
+    if x_diff > 0 {
+        new_pos.x++
+    } else if x_diff < 0 {
+        new_pos.x--
+    }
+
+    if y_diff > 0 {
+        new_pos.y++
+    } else if y_diff < 0 {
+        new_pos.y--
+    }
+
+    return new_pos
+}
+
 type PlanckRope struct {
-    head Pos
-    tail Pos
+    head *Pos
+    knots []Pos
     visited map[Pos]bool
 }
 
-func NewPlanckRope() PlanckRope {
+func NewPlanckRope(n int) PlanckRope {
     v := make(map[Pos]bool)
     v[Pos{0, 0}] = true
+
+    k := make([]Pos, n)
+    for i := 0; i < n; i++ {
+        k[i] = Pos{0, 0}
+    }
+
     return PlanckRope {
-        Pos{0, 0},
-        Pos{0, 0},
+        &k[0],
+        k,
         v,
     }
 }
 
 func (pr *PlanckRope) move(m Move) {
     for i := 0; i < m.length; i++ {
-        prev_head_pos := pr.head
-
         switch m.direction {
-        case 'U': pr.head = Pos{pr.head.x, pr.head.y - 1}
-        case 'R': pr.head = Pos{pr.head.x + 1, pr.head.y}
-        case 'D': pr.head = Pos{pr.head.x, pr.head.y + 1}
-        case 'L': pr.head = Pos{pr.head.x - 1, pr.head.y}
+        case 'U':
+            *pr.head = Pos{pr.head.x, pr.head.y - 1}
+        case 'R':
+            *pr.head = Pos{pr.head.x + 1, pr.head.y}
+        case 'D':
+            *pr.head = Pos{pr.head.x, pr.head.y + 1}
+        case 'L':
+            *pr.head = Pos{pr.head.x - 1, pr.head.y}
         }
 
-        if !pr.head.touching(pr.tail) {
-            pr.tail = prev_head_pos
-            pr.visited[pr.tail] = true
+        for ki := 1; ki < len(pr.knots); ki++ {
+            if !pr.knots[ki - 1].touching(pr.knots[ki]) {
+                pr.knots[ki] = pr.knots[ki].step_towards(pr.knots[ki -1])
+                if ki == len(pr.knots) - 1 {
+                    pr.visited[pr.knots[ki]] = true
+                }
+            }
         }
     }
 }
@@ -117,17 +153,20 @@ func main() {
     args.parse()
 
     moves := parse_input(args.input)
-    pr := NewPlanckRope()
 
     switch args.part {
-    case 1: {
+    case 1:
+        pr := NewPlanckRope(2)
         for _, move := range moves {
             pr.move(move)
         }
         fmt.Println(len(pr.visited))
-    }
     case 2:
-        ;
+        pr := NewPlanckRope(10)
+        for _, move := range moves {
+            pr.move(move)
+        }
+        fmt.Println(len(pr.visited))
     default:
         log.Printf("error: part %d not implemented\n", args.part)
     }
